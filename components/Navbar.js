5f40e9c4e876d6b29c875c513ba47ee72d845fac
Mohammad Barati <m.mohammadbarati.b@gmail.com>
2022-10-29 23:54:43 +0330

    create cart page

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
@@ -41,23 +42,27 @@ const Navbar = () => {
           <li>Menu</li>
           <li>Contact</li>
           <li className="relative">
-            <span className="absolute -top-3 -right-3 p-1 grid place-content-center bg-white min-w-[1.5rem] h-[1.5rem] rounded-full">
-              11
-            </span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-7 h-7"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-              />
-            </svg>
+            <Link href="/cart">
+              <a>
+                <span className="absolute -top-3 -right-3 p-1 grid place-content-center bg-white min-w-[1.5rem] h-[1.5rem] rounded-full">
+                  11
+                </span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-7 h-7"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                  />
+                </svg>
+              </a>
+            </Link>
           </li>
         </ul>
       </nav>
---
+++ pages/cart/index.js
@@ -0,0 +1,64 @@
+const Cart = () => {
+  return (
+    <section className="wrapper flex flex-col gap-3 sm:flex-row">
+      <ul className="cart-items flex-1 text-base text-neutral-500">
+        <li className="flex justify-evenly items-center py-2 sm:justify-between">
+          <img src="./images/pizza.png" alt="pizza" className="w-20" />
+          <div className="flex flex-col p-1 border-x border-neutral-300 md:flex-1 md:flex-row md:justify-evenly md:border-none">
+            <h4 className="text-neutral-800">Pepperoni</h4>
+            <p>Extra Cheese, Garlic Sauce</p>
+          </div>
+          <div className="flex flex-col md:flex-1 md:flex-row md:justify-evenly">
+            <p>Total: 2</p>
+            <p>$37.60</p>
+          </div>
+        </li>
+        <li className="flex justify-evenly items-center py-2 sm:justify-between">
+          <img src="./images/pizza.png" alt="pizza" className="w-20" />
+          <div className="flex flex-col p-1 border-x border-neutral-300 md:flex-1 md:flex-row md:justify-evenly md:border-none">
+            <h4 className="text-neutral-800">Pepperoni</h4>
+            <p>Extra Cheese, Garlic Sauce</p>
+          </div>
+          <div className="flex flex-col md:flex-1 md:flex-row md:justify-evenly">
+            <p>Total: 2</p>
+            <p>$37.60</p>
+          </div>
+        </li>
+        <li className="flex justify-evenly items-center py-2 sm:justify-between">
+          <img src="./images/pizza.png" alt="pizza" className="w-20" />
+          <div className="flex flex-col p-1 border-x border-neutral-300 md:flex-1 md:flex-row md:justify-evenly md:border-none">
+            <h4 className="text-neutral-800">Pepperoni</h4>
+            <p>Extra Cheese, Garlic Sauce</p>
+          </div>
+          <div className="flex flex-col md:flex-1 md:flex-row md:justify-evenly">
+            <p>Total: 2</p>
+            <p>$37.60</p>
+          </div>
+        </li>
+        <li className="flex justify-evenly items-center py-2 sm:justify-between">
+          <img src="./images/pizza.png" alt="pizza" className="w-20" />
+          <div className="flex flex-col p-1 border-x border-neutral-300 md:flex-1 md:flex-row md:justify-evenly md:border-none">
+            <h4 className="text-neutral-800">Pepperoni</h4>
+            <p>Extra Cheese, Garlic Sauce</p>
+          </div>
+          <div className="flex flex-col md:flex-1 md:flex-row md:justify-evenly">
+            <p>Total: 2</p>
+            <p>$37.60</p>
+          </div>
+        </li>
+      </ul>
+      <div>
+        <div className="sticky top-20 p-6 bg-neutral-600 text-white rounded-lg">
+          <h5 className="mb-3 text-center text-2xl">
+            TOTAL: <span>$94.65</span>
+          </h5>
+          <button className="block m-auto p-3 py-1 bg-white text-primary rounded">
+            Check Out Now!
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Cart;
