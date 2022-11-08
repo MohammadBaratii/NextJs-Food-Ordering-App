59f1ee0ade5882919bb58200204f778f47e925df
Mohammad Barati <m.mohammadbarati.b@gmail.com>
2022-11-08 13:34:59 +0330

    create menu page

---
+++ pages/menu/index.js
@@ -0,0 +1,18 @@
+import PizzaList from "../../components/PizzaList";
+
+const Menu = ({ pizzas }) => {
+  return <PizzaList pizzas={pizzas} />;
+};
+
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:3000/api/products");
+  const data = await res.json();
+
+  return {
+    props: {
+      pizzas: data.products,
+    },
+  };
+}
+
+export default Menu;
